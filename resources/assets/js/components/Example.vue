b22dbe623280181632732d0442ee0039864c50ad
David Hopson <mantle@hotmail.co.uk>
2017-08-02 00:06:57 +0100

    First

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Amount</th>
+                        <th>Ratio</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="task in list">
+                        <td v-for="item in task">
+                            {{item}}
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                message: '',
+                list: []
+            }
+        },
+        mounted() {
+            console.log("HUR")
+            this.fetchTaskList();
+        },
+        methods: {
+            fetchTaskList: function () {
+                this.$http.get('/api/exchange').then(function (response) {
+                    console.log(response);
+                    this.list = response.data;
+                })
+            }
+        }
+    }
+</script>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Symfony\Component\DomCrawler\Crawler;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function() {
+    return view('home');
+});
+
+Route::group(['middleware' => 'api', 'prefix' => 'api'], function() {
+    Route::get('/exchange', function() {
+        $crawler = new Crawler(file_get_contents(__DIR__.'\test.html'));
+        $items = array();
+        $crawler = $crawler->filter('table');
+
+        $i = 0;
+        foreach ($crawler->children() as $c)  {
+            if ($i > 0) {
+                $test = explode('                ', $c->nodeValue);
+                array_shift($test); array_pop($test); array_pop($test);
+                $items[] = $test;
+            }
+            $i++;
+        }
+
+        return response()->json($items);
+    });
+});
