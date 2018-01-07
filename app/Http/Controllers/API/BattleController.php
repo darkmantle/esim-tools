ffd5a0cd88e0b32436207a69313875c80fa8f32c
David Hopson <mantle@hotmail.co.uk>
2018-01-07 22:29:02 +0000

    Battle timers

--- app/Http/Controllers/API/BattleController.php
+++ app/Http/Controllers/API/BattleController.php
@@ -11,6 +11,7 @@ use Symfony\Component\DomCrawler\Crawler;
 class BattleController extends Controller {
 	function getAll($server, $country) {
 		$crawler = new Crawler(file_get_contents('http://'.$server.'.e-sim.org/battles.html?countryId=' . $country));
+        //$crawler = new Crawler(file_get_contents(__DIR__ . '/test.html'));
 
 		$rows = array();
 		$tr_elements = $crawler->filterXPath('//table/tr');
@@ -29,13 +30,20 @@ class BattleController extends Controller {
 
 			if (str_contains($tds[0], "Practice ")) continue;
 
+			$test = $tds[0];
+            preg_match('/.+?(?=\))/', explode('getHours() + ', $test)[1], $hours);
+			preg_match('/.+?(?=\))/', explode('getMinutes() + ', $test)[1], $minutes);
+            preg_match('/.+?(?=\))/', explode('getSeconds() + ', $test)[1], $seconds);
+
 			$nf = new \NumberFormatter("en_EN", \NumberFormatter::DECIMAL);
 
 			$battle = new \stdClass();
 			$battle->region = explode("Subsidies", $tds[0])[0];
 			$battle->score = $tds[1];
+			$battle->time = $hours[0]." hour ".$minutes[0]." minutes";
 			$battle->damage = $nf->parse($tds[2], \NumberFormatter::TYPE_INT32);
 			$rows[] = $battle;
+
 		}
 
 		return response()->json($rows);
